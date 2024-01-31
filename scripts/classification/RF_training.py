f54bba0bd54de88ef8345b16a403a147915b36a4
Luis Kremer <luis1.kremer@gmail.com>
2024-01-31 12:10:39 +0100

    rf classification on subsampled pcds

---
+++ scripts/classification/RF_training.py
@@ -0,0 +1,98 @@
+import logging
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score
+import joblib
+from pathlib import Path
+import sys
+
+sys.path.append(str(Path(__file__).parent.parent.absolute()))
+
+import utils.logger as lgr
+from utils.header_dict import header_dict
+
+# Set the root directory to the project directory (.../pcd_mesh)
+root_dir = Path(__file__).resolve().parent.parent.parent
+print(f'Root directory: {root_dir}')
+
+ # Set up the logger
+logger = lgr.logger_setup('rf_training', 
+                            Path(root_dir) / "data/logs/rf_training.log")
+
+
+def load_data(file_paths):
+    """
+    Load data from a CSV file or a list of CSV files and return a pandas DataFrame.
+
+    Args:
+        file_paths (str or list of str): The path(s) to the CSV file(s).
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing the loaded data.
+    """
+    print(f'Loading data')
+    if isinstance(file_paths, str):
+        file_paths = [file_paths]
+
+    data_frames = []
+    for file_path in file_paths:
+        data = pd.read_csv(file_path, delimiter=' ', header=None,
+                           names=list(header_dict.keys()))
+        data_frames.append(data)
+
+    return pd.concat(data_frames, axis=0)
+
+
+def train_and_test_model(X_train, y_train, X_test, y_test, model_path, logger):
+    # Set up random forest classifier
+    logger.info("Setting up random forest classifier...")
+    # Load the already trained model if it exists
+    if model_path.exists():
+        rf = joblib.load(model_path)
+    else:
+        rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+    # Train the model
+    logger.info("Training the model...")
+    rf.fit(X_train, y_train)
+
+    # Predict on test set
+    logger.info("Predicting on test set...")
+    y_pred = rf.predict(X_test)
+
+    # Calculate accuracy score
+    accuracy = accuracy_score(y_test, y_pred)
+    logger.info(f"Accuracy score: {accuracy}")
+
+    # Save the model
+    joblib.dump(rf, model_path)
+    logger.info("Model saved successfully.")
+
+
+if __name__ == "__main__":
+    # Load training data
+    data_dir = Path(root_dir) / "data/06_subsampling/SiteA_Scans_Global_I_RGB_RHV"
+    model_path = Path(root_dir) / "data/models/random_forest_SiteA.joblib"
+    data_paths = list(data_dir.glob('*.txt'))
+    data = load_data(data_paths)
+    
+
+    # Split data into features and labels
+    X = data.iloc[:, 9:-1].values
+    y = data.iloc[:, -1].values
+
+    # Split data into training and testing sets using 5-fold cross validation
+    kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+   
+    for index, (train_index, test_index) in enumerate(kf.split(X, y)):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        
+        logger.info(f"{index}")
+        logger.info(f"X_train shape (training): {X_train.shape}")
+        logger.info(f"y_train shape (training): {y_train.shape}")
+        logger.info(f"X_test shape (validation): {X_test.shape}")
+        logger.info(f"y_test shape (validation): {y_test.shape}")
+
+        train_and_test_model(X_train, y_train, X_test, y_test, model_path, logger)
