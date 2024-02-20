6ae571761262ba2480b598aca600220a5c35b6d6
Luis Kremer <luis1.kremer@gmail.com>
2024-02-20 09:31:06 +0100

    classification of segments

---
+++ scripts/utils/segment_classification.py
@@ -0,0 +1,54 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+import pandas as pd
+import joblib
+from pathlib import Path
+import numpy as np
+from numba import njit, prange
+import numba
+
+def evaluate_classifier(rf_model, features, labels):
+    predicted_labels = rf_model.predict(features)
+    accuracy = accuracy_score(labels, predicted_labels)
+    precision = precision_score(labels, predicted_labels, average='weighted')
+    recall = recall_score(labels, predicted_labels, average='weighted')
+    f1 = f1_score(labels, predicted_labels, average='weighted')
+    cnf_matrix = confusion_matrix(labels, predicted_labels)
+    
+    return predicted_labels, accuracy, precision, recall, f1, cnf_matrix
+
+
+def segment_classification(pcd_subsampled: np.ndarray, 
+                           model_filepath: str,
+                           metrics_output_filepath: str, 
+                           cnfmatrix_output_path: str):    
+    features = pcd_subsampled[:, 9:-2] # ignore the first 9 columns (position) and the last 2 columns (segment ids, labels)
+    labels = pcd_subsampled[:, -1]
+    
+    rf_model = joblib.load(model_filepath)
+    
+    predicted_labels, acc, prec, rec, f1, cnf_matrix = evaluate_classifier(rf_model, features, labels)
+    
+    with metrics_output_filepath.open('a') as f:
+        f.write("accuracy,precision,recall,f1,filename\n")
+        f.write(f"{acc},{prec},{rec},{f1},{metrics_output_filepath.name}\n")
+    
+    np.savetxt(cnfmatrix_output_path, cnf_matrix, delimiter=',', fmt='%u')
+    
+    return predicted_labels
+
+
+@njit(parallel=True)
+def unfold_labels(pcd: np.ndarray, 
+                  pcd_subs_predicted_labels: np.ndarray,
+                  indices_per_class):
+    predicted_labels = np.zeros(pcd.shape[0])
+
+    for i in prange(pcd_subs_predicted_labels.shape[0]):
+        segment_indices = indices_per_class[i]
+        predicted_labels[segment_indices] = pcd_subs_predicted_labels[i]
+
+    return predicted_labels
+
+def assign_labels(pcd: np.ndarray, 
+                  predicted_labels: np.ndarray):
+    return np.c_[pcd[:, :12], predicted_labels]
